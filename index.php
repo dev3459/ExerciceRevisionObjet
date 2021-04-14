526b6e9a534534151e9675c5eb838d2a57b7714e
dev3459 <test45@outlook.fr>
2021-04-15 01:42:27 +0200

    Ajout du formulaire et du design CSS

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exercice de révision</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <div id="contains">
+        <h1>Inscription</h1>
+        <form action="">
+            <input type="text" name="name" id="name" placeholder="Votre nom" minlength="3" maxlength="50" required>
+            <input type="text" name="firstname" id="firstname" placeholder="Votre prénom" minlength="3" maxlength="50">
+            <input type="email" name="email" id="email" placeholder="Votre email" required>
+            <input type="submit" value="S'inscrire">
+        </form>
+    </div>
+</body>
+</html>
